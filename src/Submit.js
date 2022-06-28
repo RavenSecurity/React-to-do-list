282d301ae2255473444e1db6aee2c030833a1300
RayaneKaddoury <rayanekaddoury@gmail.com>
2022-06-28 15:18:28 +0200

    day 1 tasks done

--- src/Submit.js
+++ src/Submit.js
@@ -3,5 +3,6 @@ import React from 'react'
 export default function Submit() {
   return (
     <button className="mr-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Add Tasks</button>
+    
   )
 }
