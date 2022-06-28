60c64c2aaaa1cbd33080665c32d1c1ee6f1634d1
RayaneKaddoury <rayanekaddoury@gmail.com>
2022-06-28 14:02:40 +0200

    tailwind init

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   const [todos, setTodos] = useState(['Todo 1', 'Todo 2'])
   return (
     <>
+    <h1 className="text-3xl font-bold underline flex justify-center">To-do List</h1>
     <input type="text" />
     <button>Add Tasks</button>
     <button>Clear Completed</button>
--- src/index.js
+++ src/index.js
@@ -5,6 +5,10 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <style>
+      @tailwind base;
+      @tailwind components;
+      @tailwind utilities;</style>
     <App />
   </React.StrictMode>
 );
