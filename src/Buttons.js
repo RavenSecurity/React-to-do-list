18b5f6f124c0ac50ec7ab51680b3699e449edbb5
RayaneKaddoury <rayanekaddoury@gmail.com>
2022-07-02 20:25:08 +0200

    props retrieved

--- src/Buttons.js
+++ src/Buttons.js
@@ -1,19 +1,18 @@
 // Each button has props (colors, callbacks (fonction associé à l'événement), content (text) )
 
 import React from 'react'
-import App from './App'
 
-export default function Buttons( props, {handleClearTodos, handleAddTodo} ) {
-  // todoNameRef, setTodos, todos
-  console.log(props)
+export default function Buttons( props ) {
   const todoNameRef = props.todoNameRef
+  
+  console.log(todoNameRef)
 
  
   
   return (
     <div>
-      <button onClick={handleAddTodo} className="mr-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded h-10">Add Tasks</button>
-      <button onClick={handleClearTodos} className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded h-10">Clear Completed</button>
+      <button onClick={props.handleAddTodo} className="mr-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded h-10">Add Tasks</button>
+      <button onClick={props.handleClearTodos} className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded h-10">Clear Completed</button>
     </div>
   )
 }
